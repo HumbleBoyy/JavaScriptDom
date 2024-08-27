efc6c16a09bf45810d0815df6ac8dd27c1c9485d
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-08-27 15:28:55 +0500

    Second commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// EXIMINE THE DOCUMENT OBJECT //
+
+// dir() show whole document object
+// console.dir(document)
+
+// Checks Domain
+console.log(document.domain);
+
+// Checks URL
+console.log(document.URL)
+
+console.log(document.title)
+// document.title = 1234
+console.log(document.doctype)
